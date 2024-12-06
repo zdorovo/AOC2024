9d413bcb080563586304fcd49d4b5f484995573b
Daniel Smolkin <daniel.smolkin@gmail.com>
2024-12-06 11:03:47 -0500

    fixed duplication problem w sol2 script

--- 3/cpp/sol2.sh
+++ 3/cpp/sol2.sh
@@ -2,6 +2,6 @@
 
 
 grep -e 'mul([0-9]\+,[0-9]\+)' -e "don't()" -e "do()" -o $1 > tmp.txt
-sed -e "s/mul(\([0-9]\+\),\([0-9]\+\))/\1 \2/gp" tmp.txt > out
+sed -e "s/mul(\([0-9]\+\),\([0-9]\+\))/\1 \2/g" tmp.txt > out
 cat out | ./adder
 
